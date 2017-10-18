a3d88e123ae9a0c68f490f2ead9a3b7ad475bdc5
pvp <petervanpansen@gmail.com>
2017-10-18 21:22:26 +0200

    Enabled mosquitto_log_printf

--- log.c
+++ log.c
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) 2013 Jan-Piet Mens <jp@mens.de>
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
  * 3. Neither the name of mosquitto nor the names of its
  *    contributors may be used to endorse or promote products derived from
  *    this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -52,9 +52,9 @@ void _log(int priority, const char *fmt, ...)
 
 	va_start(va, fmt);
 
-// #if (LIBMOSQUITTO_MAJOR > 1) || ((LIBMOSQUITTO_MAJOR == 1) && (LIBMOSQUITTO_MINOR >= 4))
-#if (0)
+#if (LIBMOSQUITTO_MAJOR > 1) || ((LIBMOSQUITTO_MAJOR == 1) && (LIBMOSQUITTO_MINOR >= 4))
 	mosquitto_log_printf(priority, fmt, va);
+	va_end(va);
 #else
 	fprintf(stderr, "%ld: |-- ", now);
 	vfprintf(stderr, fmt, va);
@@ -62,7 +62,7 @@ void _log(int priority, const char *fmt, ...)
 	fflush(stderr);
 	va_end(va);
 #endif
-}	
+}
 
 void _fatal(const char *fmt, ...)
 {
